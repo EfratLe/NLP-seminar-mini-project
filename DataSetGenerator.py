802bbe1b9d29703d028a3d625f696230aa2316ca
EfratLe <efrat3232@gmail.com>
2021-12-21 02:26:30 +0200

    transformation, inject typos and punctoation

--- DataSetGenerator.py
+++ DataSetGenerator.py
@@ -9,13 +9,14 @@ import csv
 # save
 import random
 from typing import List
-
+import math
+import string
 import numpy as np
 
 
 class DataSetGenerator:
     def __init__(self, sourceDateSet, similarWordsDataSet,
-                 configs={"number_negative_data_same_category": 200, "number_negative_data_different_category": 200}):
+                 configs={"number_negative_data_same_category": 200, "number_negative_data_different_category": 200, "number_positive_data_same_category":200}):
         self.source_dataset = []
         with open(sourceDateSet) as source:
             for row in csv.reader(source, delimiter=','):
@@ -32,7 +33,10 @@ class DataSetGenerator:
         # initialize configs
         self.number_negative_data_same_category = configs.get("number_negative_data_same_category")
         self.number_negative_data_different_category = configs.get("number_negative_data_different_category")
-
+        self.number_positive_data_same_category =configs.get("number_positive_data_same_category")
+        self.synonym_switching_rate = 0.9
+        self.typos_injection_rate = 0.9
+        self.punctuation_insetion_rate = 0.3
         self.punctuation = '!\'#$%&\'()*+, -./:;<=>?@[\]^_`{|}~'
         pass
 
@@ -51,12 +55,10 @@ class DataSetGenerator:
         :return: dataset
         """
         positive_data = self.transforms()
-
         # transforms
         negative_data = self.create_negative_data()
         all_data = positive_data + negative_data
         random.shuffle(all_data)
-        print(all_data)
         with open(destDataSet, 'w', newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',')
             for row in all_data:
@@ -89,14 +91,46 @@ class DataSetGenerator:
         return all_negative_samples
 
     def transforms(self) -> List:  # efrat
-        # for sentence:
-        # randomly select words for switching
+        """
+        create positive data by slightly changing sentences.
+        :return: all positive samples list
+        """
+        all_positive_samples = []
+        category_data_dict = {}
+        for data in self.source_dataset:
+            if category_data_dict.get(data[1]) is None:
+                category_data_dict[data[1]] = [data[0]]
+            else:
+                category_data_dict[data[1]] += [data[0]]
+        samples = random.sample(self.source_dataset, self.number_positive_data_same_category)
+        for sample in samples:
+            new_sentence = sample[0]
+            if random.random() < self.synonym_switching_rate:
+                new_sentence = self.switchingSynonym(new_sentence)
+            if random.random() < self.typos_injection_rate:
+                new_sentence = self.injectTypos(new_sentence)
+            if random.random() < self.punctuation_insetion_rate:
+                new_sentence = self.punctuationInsertion(new_sentence)
+            all_positive_samples.append([sample[0], new_sentence, 1])
+        return all_positive_samples
 
-        return []
-
-    def injectTypos(self, word):  # efrat
-        # return string
-        pass
+    def injectTypos(self, sentence):  # efrat
+        """
+        Randomly adds typos to the given sentence.
+        :param sentence:
+        :return: sentence with typos
+        """
+        def typo(x):
+            if random.random() < 0.01: #insertion right
+                return random.choice(string.ascii_letters+string.digits) +x
+            if random.random() < 0.01:  # insertion left
+                return x + random.choice(string.ascii_letters+string.digits)
+            if random.random() < 0.01:  #replace
+                return random.choice(string.ascii_letters+string.digits)
+            if random.random() < 0.01:  #deletion
+                return ""
+            return x
+        return ''.join(map(typo, sentence))
 
     def switchingSynonym(self, sentence: str) -> str:  # adi
         """
@@ -118,8 +152,14 @@ class DataSetGenerator:
             if index != -1:
                 sentance_list[i] = self.similarity_data_row_0[index]
                 break
-        print(sentance_list)
         return " ".join(sentance_list)
 
-    def panctuatuionInsertion(self, sentence):  # efrat
-        pass
+    def punctuationInsertion(self, sentence):  # efrat
+        """
+        Randomly adds punctuation to the given sentence. Prefers adding punctuation next to spaces.
+        :param sentence:
+        :return: sentence with punctuation insertions
+        """
+        def randPunctuation():
+            return random.choice(string.punctuation)
+        return ''.join(map(lambda x: (randPunctuation()+x if  random.random() < 0.5 else x+ randPunctuation()) if ((x.isspace() and random.random() < 0.05) or random.random() < 0.005) else x, sentence))
